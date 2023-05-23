200d7188164503392149803840f0dd4a6d140485
levanxuan5518@gmail.com <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>
2023-05-23 15:22:12 +0700

    update field password

--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -13,10 +13,12 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 
 @Entity
 @Table(name = "user")
+@Data
 public class User {
 
 	@Id
@@ -37,37 +39,7 @@ public class User {
 	@JsonIgnore
     private Cart cart;
 	
-	public Cart getCart() {
-		return cart;
-	}
 
-	public void setCart(Cart cart) {
-		this.cart = cart;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 
 }
