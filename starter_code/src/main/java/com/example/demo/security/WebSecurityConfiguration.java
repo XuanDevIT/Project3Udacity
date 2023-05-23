78dda6f84b768ff94ef12db327d0f9fe15153d3e
levanxuan5518@gmail.com <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>
2023-05-23 16:36:08 +0700

    fix bug security

--- starter_code/src/main/java/com/example/demo/security/WebSecurityConfiguration.java
+++ starter_code/src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -1,13 +1,12 @@
 package com.example.demo.security;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
